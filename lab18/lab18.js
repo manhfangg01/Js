3ba8d070178417226bbd86dd65a09c77d1b176d6
PhanVanManh <imseirousmanh@gmail.com>
2025-11-27 20:21:11 +0700

    lab 18

---
+++ lab18/lab18.js
@@ -0,0 +1,22 @@
+// KIỂU NGUYÊN THỦY
+// number không phân biệt nguyên thưc
+const n1 = 1;
+const n2 = 1.0;
+
+// string
+
+const name = "manh"; // nên dùng
+const surname = "phan";
+const lop = `lop ${n1}`; // format string
+
+// boolean
+
+// undefined
+let bla;
+console.log(bla);
+
+// xác định kiểu
+
+console.log(typeof bla);
+
+// ===================================
